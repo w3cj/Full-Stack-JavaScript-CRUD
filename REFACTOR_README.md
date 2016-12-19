8e788f22e20ddea2c35b3615eda4fc90b1701f67
CJ R <hello@cjr.co.de>
2016-12-19 07:42:23 -0700

    :scissors: Remove codes. :books: Update README

--- REFACTOR_README.md
+++
@@ -1,6 +0,0 @@
-# Refactor an express router file
-
-* [x] Update validations
-* [x] Move common methods to lib folder
-* [x] Move knex queries to db folder
-* [x] Create reusable function to send error response
--- client/client.js
+++
@@ -1,8 +0,0 @@
-$(() => {
-  $.get(API_URL).then(todos => {
-		const $todos = $('.todos');
-	  todos.forEach(todo => {
-			$todos.append(`<a href="/single.html?id=${todo.id}" class="list-group-item">${todo.title}</a>`);
-  	});
-  });
-});
